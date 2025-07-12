1f64c2f480488c2b15d008be2812abbb407f6041
Paul <paul@paulbsd.com>
2025-07-12 19:37:46 +0200

    chore: let's breath

--- src/core/core.go
+++ src/core/core.go
@@ -19,6 +19,7 @@ const lastmodifiedFormat = `Mon, 2 Jan 2006 15:04:05 MST`
 
 func GetBody(url string) (body io.ReadCloser, lastmodified time.Time, err error) {
 	lastmodified = time.Now()
+	err = fmt.Errorf("Can't access data")
 
 	if strings.HasPrefix(url, "file:///") {
 		path := strings.Replace(url, "file://", "", 1)
@@ -30,7 +31,6 @@ func GetBody(url string) (body io.ReadCloser, lastmodified time.Time, err error)
 
 		return body, lastmodified, nil
 	}
-	err = fmt.Errorf("Can't access data")
 
 	return
 }
@@ -40,10 +40,12 @@ func GetLocalFile(path string) (body io.ReadCloser, lastmodified time.Time, err 
 	if err != nil {
 		log.Println(err)
 	}
+
 	fstat, err := file.Stat()
 	if err != nil {
 		log.Println(err)
 	}
+
 	lastmodified = fstat.ModTime()
 	body = file
 
@@ -55,13 +57,13 @@ func GetRemoteFile(url string) (body io.ReadCloser, lastmodified time.Time, err 
 	if err != nil {
 		log.Println(err)
 	}
+
 	lmstr := res.Header.Get("Last-Modified")
 	if err != nil {
 		log.Println(err)
 	}
 
 	lastmodified, err = time.Parse(lastmodifiedFormat, lmstr)
-
 	if err != nil {
 		return nil, lastmodified, err
 	}
@@ -96,6 +98,7 @@ func HandleStringOrDomain(cfg *config.Cfg, blocklist *config.Blocklist) (err err
 				log.Println(err)
 				log.Fatalf("can't open file %s\n", tmpfile)
 			}
+
 			defer writer.Close()
 
 			for fileScanner.Scan() {
@@ -106,6 +109,7 @@ func HandleStringOrDomain(cfg *config.Cfg, blocklist *config.Blocklist) (err err
 					handled++
 				}
 			}
+
 			log.Printf("%d domains/strings handled for url %s\n", handled, blocklist.URL)
 		}
 
@@ -132,10 +136,15 @@ func HandleIP(cfg *config.Cfg, dbname string, blocklist *config.Blocklist) (err 
 		fileScanner.Split(bufio.ScanLines)
 
 		env, err := lmdb.NewEnv()
+		if err != nil {
+			log.Println(err)
+		}
+
 		err = env.SetMapSize(100 * 1024 * 1024)
 		if err != nil {
 			log.Println(err)
 		}
+
 		err = env.SetMaxDBs(1)
 		if err != nil {
 			log.Println(err)
@@ -149,11 +158,16 @@ func HandleIP(cfg *config.Cfg, dbname string, blocklist *config.Blocklist) (err 
 
 		err = env.Update(func(txn *lmdb.Txn) (err error) {
 			dbi, err := txn.CreateDBI(dbname)
+			if err != nil {
+				log.Println(err)
+			}
+
 			err = txn.Drop(dbi, false)
 			if err != nil {
 				log.Println(err)
 				return
 			}
+
 			return
 		})
 		if err != nil {
@@ -162,6 +176,7 @@ func HandleIP(cfg *config.Cfg, dbname string, blocklist *config.Blocklist) (err 
 
 		err = env.Update(func(txn *lmdb.Txn) (err error) {
 			var handled int
+
 			dbi, err := txn.CreateDBI(dbname)
 			if err != nil {
 				log.Println(err)
@@ -174,6 +189,7 @@ func HandleIP(cfg *config.Cfg, dbname string, blocklist *config.Blocklist) (err 
 				if len(ipitem) == 0 {
 					continue
 				}
+
 				if strings.Contains(ipitem, "/") {
 					upper, lower, err = convertCIDR(ipitem, cfg.Config.IPv4MaxCidrValue, cfg.Config.IPv6MaxCidrValue)
 					if err != nil {
@@ -186,16 +202,21 @@ func HandleIP(cfg *config.Cfg, dbname string, blocklist *config.Blocklist) (err 
 						log.Println(err)
 						continue
 					}
+
 					lower = upper
 				}
+
 				err = txn.Put(dbi, upper, lower, 0)
 				if err != nil {
 					log.Println(err)
 					return
 				}
+
 				handled++
 			}
+
 			log.Printf("%d ips handled for url %s\n", handled, blocklist.URL)
+
 			return
 		})
 	} else {
